2f5909381c8fa259e59000ce71b5e93cdd97a06f
Christian <chris23681@gmail.com>
2021-11-21 20:36:21 -0800

    Fixed Homepage Display

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const header = new Header('Trial by Coffee', ['Home', 'Menu', 'About Us']);
 const aboutUs = new AboutUs('Trial by Coffee', '123 Somewhere St, Los Angeles CA 32004', '(901)789-2345', 'trialbycoffee@gmail.com');
 
 main.classList.add('mainSection');
-main.appendChild(aboutUs.makePage());
+main.appendChild(displayHomepage());
 
 let footer = new Footer();
 let footerLinks = [['Our Partners', '#'], ['Contact Us', '#'], ['About Us', '#']]
